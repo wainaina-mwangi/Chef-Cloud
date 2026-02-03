476c6143bb1b0a5d0c607a69cd5140f98eb2906a
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-02-03 17:23:45 +0300

    adding event listeners

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Main() {
+    function handleSubmit() {
+        alert("hurray");
+        
+    }
   return (
     <main>
-      <form action="" className="add-form">
+      <form onSubmit={handleSubmit} action="" className="add-form">
         {/* <label htmlFor="#">Add Ingridients</label> */}
         <input type="text" placeholder="e.g Capuchino " />
         <button>Add Ingridients</button>
