4373cf6037d162b6a49e4b8c787f1b9caf22e295
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-02-04 14:32:45 +0300

    dealing with events and prevent default

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 function Main() {
-    function handleSubmit() {
-        alert("hurray");
+    function handleSubmit(e) {
+      // e is for event
+      // e.preventDefault stops the form for refresh
+        e.preventDefault();
         
     }
   return (
     <main>
       <form onSubmit={handleSubmit} action="" className="add-form">
         {/* <label htmlFor="#">Add Ingridients</label> */}
-        <input type="text" placeholder="e.g Capuchino " />
+        <input type="text" placeholder="e.g Capuchino " name="indgridients" />
         <button>Add Ingridients</button>
       </form>
     </main>
